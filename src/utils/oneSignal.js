4afd7e943fdac554e641213522bf4d766c707802
Fanau <Lyfanau99@gmail.com>
2025-06-01 13:28:58 +0700

    modifie oneSignal.js

--- src/utils/oneSignal.js
+++ src/utils/oneSignal.js
@@ -1,16 +1,39 @@
 import OneSignal from 'react-onesignal';
 
+// export default async function initOneSignal(externalUserId) {
+//     await OneSignal.init({
+//         appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
+//         serviceWorkerPath: '/OneSignalSDKWorker.js',
+//         notifyButton: {
+//             enable: true,
+//         },
+//         allowLocalhostAsSecureOrigin: true,
+//     });
+
+//     if (externalUserId) {
+//         await OneSignal.User.addAlias("external_id", externalUserId);
+//     }
+// }
+
+
 export default async function initOneSignal(externalUserId) {
-    await OneSignal.init({
-        appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
-        serviceWorkerPath: '/OneSignalSDKWorker.js',
-        notifyButton: {
-            enable: true,
-        },
-        allowLocalhostAsSecureOrigin: true,
+  if (typeof window === "undefined") return; // Don't run on server
+
+  const OneSignal = window.OneSignal || [];
+
+  OneSignal.push(() => {
+    OneSignal.init({
+      appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
+      serviceWorkerPath: "/OneSignalSDKWorker.js",
+      notifyButton: {
+        enable: true,
+      },
+      allowLocalhostAsSecureOrigin: true,
     });
 
     if (externalUserId) {
-        await OneSignal.User.addAlias("external_id", externalUserId);
+      OneSignal.User.addAlias("external_id", externalUserId);
     }
+  });
 }
+  
